6d5b0071a03ceaed757b76ae3019ba78e05e72d1
jonathas fonseca <jonathas09@gmail.com>
2020-07-09 17:50:33 -0300

    corigido a entity no typeorm.config

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,10 +1,17 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { TaskStatus} from './task-status.enum';
+import { TaskStatus } from './task-status.enum';
 
 @Entity()
 export class Task extends BaseEntity {
-  @PrimaryGeneratedColumn() id: number;
-  @Column() title: string;
-  @Column() description: string;
-  @Column() status: TaskStatus;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
 }
