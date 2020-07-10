2b5fd77a4b0d5d4e7bf2644863aa8b1acce10277
jonathas fonseca <jonathas09@gmail.com>
2020-07-10 15:58:42 -0300

    adição de validadores de username e password

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,7 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+
 export class AuthCredentialsDto {
-  username: string;
-  password: string;
+  @IsString() @MinLength(6) @MaxLength(20) username: string;
+  @IsString() @MinLength(6) @MaxLength(15) @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z]).*$/) password: string;
 }
