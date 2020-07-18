d5ef6187c2dbf9a55de0b9406c5c2993ca5ccaa7
jonathas fonseca <jonathas09@gmail.com>
2020-07-18 00:35:53 -0300

    fazendo filtragem do user no getTasks

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -16,5 +16,9 @@ export class Task extends BaseEntity {
   @Column()
   status: TaskStatus;
 
-  @ManyToOne(type => User, user => user.tasks, { eager: false }) user: User;
+  @ManyToOne(type => User, user => user.tasks, { eager: false })
+  user: User;
+
+  @Column()
+  userId: number;
 }
